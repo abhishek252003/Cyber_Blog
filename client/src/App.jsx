7a7e1e826a11b16c73de8c05c33a94096763121a
abhishek kumar chaurasia <abhishek.chaurasia2111@gmail.com>
2024-12-07 01:23:49 +0530

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import About from "./pages/About"
+import Project from "./pages/Project"
+
+
 export default function App() {
   return (
-    <div className="text-3xl">App</div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/project" element={<Project />} />
+    </Routes>
+
+  </BrowserRouter>
   )
 }
